62318a2cf6700e8b41f96bf457dd9788a66d75e9
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-07-27 09:54:06 +0530

    Create guvi3.java

---
+++ guvi3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main(String args[])
+	{
+		int n=3;
+		if(n>3)
+		{
+		System.out.println("positive number");
+		}
+		else if(n<3)
+		{
+		System.out.println("negative number");	
+    }
+		else
+		{
+		System.out.println("zero");
+		}
+		}
+		}
