ee0228f2933a83212f64b7e34daf37848245daf8
warriors45 <41731136+warriors45@users.noreply.github.com>
2018-07-28 19:35:21 +0530

    Update guvi3.java

--- guvi3.java
+++ guvi3.java
@@ -5,12 +5,13 @@ class Ideone
 {
 	public static void main(String args[])
 	{
-		int n=3;
-		if(n>3)
+		Scanner input=new Scanner(System.in);
+		int n=input.nextInt;
+		if(n>0)
 		{
 		System.out.println("positive number");
 		}
-		else if(n<3)
+		else if(n<0)
 		{
 		System.out.println("negative number");	
     }
